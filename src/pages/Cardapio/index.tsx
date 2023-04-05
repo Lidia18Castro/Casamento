3c4d69e6793959c2631b1b646549729c80bac3d6
Gabriel <gabrielfariareis@gmail.com>
2023-04-04 22:45:42 -0300

    Inicio do projeto

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./Main.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+function Main() {
+  return (
+    <div>
+      <header>
+        <nav></nav>
+      </header>
+      <main>
+        <div className={styles.objeto}>
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
